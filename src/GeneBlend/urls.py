9f02d03166ef21b0472c3b7d2cd2bfeb4661deb2
DarynaBerezniuk <berezniukdaryna@gmail.com>
2026-02-24 01:20:11 +0200

    adjusted buttons

--- src/GeneBlend/urls.py
+++ src/GeneBlend/urls.py
@@ -21,6 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('education/', views.education, name='education'),
     path('result/', views.result),
     path('calculator/', include('calculator.urls'))
 ]
--- src/GeneBlend/views.py
+++ src/GeneBlend/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def education(request):
+    return render(request, 'education.html')
+
 def result(request):
     return render(request, 'results.html')
